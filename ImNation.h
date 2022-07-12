d3e3eff2e8a61468c84daa3cc64fd7ed36aa2bbf
ProB1 <50064486+iProB1@users.noreply.github.com>
2022-07-12 14:47:23 +0300

    fix

--- ImNation.h
+++ ImNation.h
@@ -16,13 +16,13 @@
 
 namespace ImNation {
 	void BoxProgressBar(const ImVec2& size_arg);
-	bool LoadingBar(const char* label, float value,  const ImVec2& size_arg, const ImU32& bg_col, const ImU32& fg_col);
-	bool LoadingCircle1(const char* label, float radius, int thickness, const ImU32& color);
-    void LoadingCircle(const ImVec4& fore_color, const ImVec4& back_color, float& arc_size, float& radius, float thickness);
-	void ChildSizex(float& ChildSizex, float& ChildSizex2, int tosize = 20, float& Speed = 1);
-	void ChildSizey(float& ChildSizey, float& ChildSizey2, int tosize = 20, float& Speed = 1);
-	void ChildSizex(float& ChildSizex, int tosize = 20, float& Speed = 1);
-	void ChildSizey(float& ChildSizey, int tosize = 20, float& Speed = 1);
-	void TextColored(int R, int G, int B, const char* label, float& Alpha = 255, bool tick = false, float& MaxValue = 255, float& Speed = 1.0f, ...);
+	bool LoadingBar(const char* label, float* value,  const ImVec2& size_arg, const ImU32& bg_col, const ImU32& fg_col);
+	bool LoadingCircle1(const char* label, float* radius, int* thickness, const ImU32& color);
+    void LoadingCircle(const ImVec4& fore_color, const ImVec4& back_color, float* arc_size, float* radius, float* thickness);
+	void ChildSizex(float* ChildSizex, float* ChildSizex2, int* tosize = 20, float* Speed = 1);
+	void ChildSizey(float* ChildSizey, float* ChildSizey2, int* tosize = 20, float* Speed = 1);
+	void ChildSizex(float* ChildSizex, int* tosize = 20, float* Speed = 1);
+	void ChildSizey(float* ChildSizey, int* tosize = 20, float* Speed = 1);
+	void TextColored(int R, int G, int B, const char* label, float* Alpha = 255, bool* tick = false, float* MaxValue = 255, float* Speed = 1.0f, ...);
     bool Button(const char* label, const ImVec2& size = ImVec2(0, 0));
 }
